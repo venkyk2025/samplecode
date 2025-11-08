70557d9a8c9cd4006f0281d5b385ae6778d7e878
venkyk2025 <vk25abc@herts.ac.uk>
2025-11-08 23:24:41 +0530

    Update calulator.py

--- github/calulator.py
+++ github/calulator.py
@@ -14,6 +14,18 @@ def divide(a, b):
         return "Cannot divide by zero!"
     return a / b
 
+x= float(input("enter the number"))
+r=x%2
+if r==1:
+    print("odd")
+    if x>10:
+        print("greater than num")
+else:
+    print("even")
+
+print("venky") 
+
+
 # Main program
 print("Simple Calculator")
 x = float(input("Enter first number: "))
@@ -23,3 +35,4 @@ print("Addition:", add(x, y))
 print("Subtraction:", subtract(x, y))
 print("Multiplication:", multiply(x, y))
 print("Division:", divide(x, y))
+
